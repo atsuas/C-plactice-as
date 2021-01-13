a819e1b0ca4f8c02ee75f0052cf28c66f23dd63b
as <>
2021-01-13 18:45:46 +0900

    列挙型について

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -4,54 +4,23 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    int[] numberList1 = new int[3] { 2, -4, 10 };
-    int[] numberList2 = new int[4] { 2, -4, 10, 100 };
+    //列挙型
+
+    public enum TYPE
+    {
+        STOP,
+        RIGHT,
+        LEFT
+    }
+
+    TYPE type = TYPE.STOP;
 
     void Start()
     {
-        Debug.Log(GetMaxValue(numberList1));
-        Debug.Log(GetMaxValue(numberList2));
+        if (type == TYPE.STOP)
+        {
+            Debug.Log(type);
+        }
     }
 
-    //　↓ここでGetMaxValueを作ってしまうとラクにできる
-    int GetMaxValue(int[] numberList)
-    {
-        int maxValue = numberList[0];
-        for (int i = 0; i < numberList.Length; i++)
-        {
-            if (maxValue < numberList[i])
-            {
-                maxValue = numberList[i];
-            }
-        }
-        return maxValue;
-    }
-    //　↑ここまで
-
-    // ↓１つずつ求める場合
-    int GetMaxValue1()
-    {
-        int maxValue = numberList1[0];
-        for (int i = 0; i < numberList1.Length; i++)
-        {
-            if (maxValue < numberList1[i])
-            {
-                maxValue = numberList1[i];
-            }
-        }
-        return maxValue;
-    }
-
-    int GetMaxValue2()
-    {
-        int maxvalue2 = numberList2[0];
-        for (int i = 0; i < numberList2.Length; i++)
-        {
-            if (maxvalue2 < numberList2[i])
-            {
-                maxvalue2 = numberList2[i];
-            }
-        }
-        return maxvalue2;
-    }
 }
