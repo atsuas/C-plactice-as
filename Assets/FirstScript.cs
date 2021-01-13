9348ef4f9227854c14d0e6879fb93f11f44163e9
as <>
2021-01-13 20:55:16 +0900

    クラスについて

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -4,51 +4,9 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    //1から100までの3の倍数と3のつく数字を要素とするリストを返す関数を作成
     void Start()
     {
-        List<int> threeAhoNumberList = GetThreeAhoNumberList();
-
-        foreach (int number in threeAhoNumberList)
-        {
-            Debug.Log(number);
-        }
-    }
-
-    List<int> GetThreeAhoNumberList()
-    {
-        List<int> threeAhoNumberList = new List<int>();
-        for (int i = 1; i < 101; i++)
-        {
-            if (IsThreeAhoNumber(i))
-            {
-                threeAhoNumberList.Add(i);
-            }
-
-            //if (i%3 == 0)
-            //{
-            //    threeAhoNumberList.Add(i);
-            //}
-        }
-        return threeAhoNumberList;
-    }
-    bool IsThreeAhoNumber(int number)
-    {
-        if (number%3 == 0)
-        {
-            return true;
-        }
-
-        while (number != 0)
-        {
-            if (number % 10 == 3)
-            {
-                return true;
-            }
-
-            number = number / 10;
-        }
-
-        return false;
+        PlayerModel player = new PlayerModel("koko");
+        player.SayName();
     }
 }
--- Assets/PlayerModel.cs
+++ Assets/PlayerModel.cs
@@ -2,6 +2,24 @@
 
 public class PlayerModel
 {
+    string name;
+    int hp;
+    int at;
 
+    //生成されたときに呼び出される
+    public PlayerModel()
+    {
+        name = "atsu";
+    }
+
+    public PlayerModel(string name)
+    {
+        this.name = name;
+    }
+
+    public void SayName()
+    {
+        Debug.Log(name);
+    }
 }
 
