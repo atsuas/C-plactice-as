015aea342d25a515765b410789035a8f5bd7684c
as <>
2021-01-13 12:43:21 +0900

    for、while、繰り返し処理について

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    
+    //繰り返し
+    //while
+    //for
+    //continue: 次の処理を行う
+    //break：処理を抜ける
     void Start()
     {
-        
+        int x = 0;
+        while (x < 10)
+        {
+            Debug.Log(x);
+            x++;
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i == 7)
+            {
+                break;
+            }
+            Debug.Log(i);
+        }
+
+        for (int i = 0; i < 21; i++)
+        {
+            if (i%2 == 0)
+            {
+                Debug.Log(i);
+            }
+        }
     }
 }
