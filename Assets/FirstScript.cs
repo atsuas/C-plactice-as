aa540277091c7c7c55b4affd858cb12c68b9c768
as <>
2021-01-13 11:52:10 +0900

    不要なスクリプトの削除

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -4,45 +4,9 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    // switch文
-    // >, >=, <, <=, ==, !=
+    
     void Start()
     {
-        string charactorType = "Friend";
-
-        //　演習 if文で書き換え
-        if (charactorType == "Player")
-        {
-            Debug.Log("味方だよ");
-        }
-        else if (charactorType == "Enemy")
-        {
-            Debug.Log("敵だよ");
-        }
-        else if (charactorType == "Friend")
-        {
-            Debug.Log("友達だよ");
-        }
-        else
-        {
-            Debug.Log("その他");
-        }
-
-        //switch文
-        switch (charactorType)
-        {
-            case "Player":
-            Debug.Log("味方だよ");
-            break;
-            case "Enemy":
-            Debug.Log("敵だよ");
-            break;
-            case "Friends":
-            Debug.Log("友達だよ");
-            break;
-            default:
-            Debug.Log("その他");
-            break;  
-        }
+        
     }
 }
