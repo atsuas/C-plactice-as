dae5bff56e7392759c8bd73b473a5f208b7a7713
as <>
2021-01-13 18:47:02 +0900

    不要なスクリプトの削除

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -4,23 +4,10 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    //列挙型
-
-    public enum TYPE
-    {
-        STOP,
-        RIGHT,
-        LEFT
-    }
-
-    TYPE type = TYPE.STOP;
 
     void Start()
     {
-        if (type == TYPE.STOP)
-        {
-            Debug.Log(type);
-        }
+        
     }
 
 }
