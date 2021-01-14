7a92b13196a817cbbc69f91c840a5184a992123a
as <>
2021-01-14 21:07:48 +0900

    オブジェクトの表示・非表示について

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameObj;
+    void Start()
+    {
+        //敵を消す際のスクリプト
+        //gameObj.SetActive(false);
+
+        //敵を破壊する際のスクリプト
+        Destroy(gameObj);
+    }
+
+}
