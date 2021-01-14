cf4eb3230e5211441e96ff5209124d70baedc3f8
as <>
2021-01-14 11:41:50 +0900

    当たり判定　isTriggerについて

---
+++ Assets/PanelScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelScript : MonoBehaviour
+{
+    //当たった物の名前をコンソールに表示してくれる
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    }
+}
