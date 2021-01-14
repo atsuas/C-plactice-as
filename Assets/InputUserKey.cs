54a737552667f90a4d00c00e8b942969d95df01c
as <>
2021-01-14 11:01:02 +0900

    Input　キーボード入力について

---
+++ Assets/InputUserKey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputUserKey : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+        //Debug.Log(x+","+y);
+
+        bool onSpace = Input.GetKey("space");
+        if (onSpace)
+        {
+            Debug.Log("攻撃");
+        }
+    }
+}
