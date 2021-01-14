5d2cb4ad77f766b0f6b1c4261f8213296620c563
as <>
2021-01-14 11:29:15 +0900

    Rigitbodyについて

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //transform.position += new Vector3(0.1f, 0, 0);  動かし方①
+        //rigidbody.velocity = new Vector3(1f, 0, 0);   動かし方②
+        rigidbody.AddForce(new Vector3(0, 10, 0));
+    }
+}
