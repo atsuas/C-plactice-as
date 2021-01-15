0455c0755c55bf780679b1f2513be5135ec0a8ca
as <>
2021-01-15 10:43:44 +0900

    シーンの切り替え方法

--- Assets/SceneScript.cs
+++ Assets/SceneScript.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene("Main");
     }
 
     // Update is called once per frame
